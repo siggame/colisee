7555eddca71e895e58821cc82633525f1e9e53d5
user404d <conduffql@gmail.com>
2018-02-20 13:52:30 -0600

    Add docker registry

--- deploy/prepare.sh
+++ deploy/prepare.sh
@@ -1,9 +1,11 @@
-#!/bin/bash -a -e
+#!/bin/bash -ae
 
 source deploy/.env
 envsubst < deploy/docker-compose.yml.template > docker-compose.yml
 
 for env_template in envs/*.template; do
     env=$(echo "$env_template" | cut -d'.' -f 1)
+    service=$(echo "$env" | cut -d'/' -f 2)
+    test -d "$STORAGE_BASE/$service" || mkdir -p "$STORAGE_BASE/$service"
     envsubst < $env_template > $env
 done
