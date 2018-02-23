23d7498c7423bcc7609a171082d8ed055676c470
user404d <conduffql@gmail.com>
2018-02-23 14:25:07 -0600

    Add simple colisee project generator

---
+++ templates/README.md
@@ -0,0 +1,25 @@
+# Creating a New Project
+
+## Project Type
+
+Choosing a project type will depend on the purpose of said project.
+
+* Service
+
+  * A service is an application which is a component within a larger encompassing architecture.
+
+* Library
+
+  * A library is a collection of modules which are together meant to be used as a building block for applications.
+
+## Project Name
+
+* Service
+
+  * Prefix the name of the parent project (eg. `colisee-runner` where `colisee` is the parent and `runner` is the service)
+
+* Library
+
+  * Optionally prefix the name of the parent project if the library doesn't meet general purpose needs (eg. `colisee-lib` vs. `tourneyjs`).
+
+## Generating General Project Assets
---
+++ templates/project.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+
+from argparse import ArgumentParser
+from collections import namedtuple
+from os import listdir, mkdir
+from os.path import basename, curdir, exists, isfile, join
+from re import search, sub
+from sys import exit
+from shutil import copyfile
+
+Template = namedtuple("Tempalte", ["project_name", "project_desc"])
+
+TEMPLATED_VALUES = Template(r"${PROJECT_NAME}", r"${PROJECT_DESCRIPTION}")
+
+parser = ArgumentParser(prog="colisee-project", description="Create new projects from project templates.")
+parser.add_argument("name", help="Name for the new project")
+parser.add_argument("type", choices=["library", "service"], help="Type of the new project")
+
+args = parser.parse_args()
+
+if isfile(args.name) or exists(args.name):
+    print("File or directory {} already exists".format(args.name))
+    exit(1)
+
+print("Please enter a short description of this project:\n")
+
+description = input()
+
+while description == "":
+    print("Are you sure you want to leave the description blank? (y/n)")
+    response = input()
+
+    if response == "n":
+        description = input()
+    elif response == "y":
+        break
+    else:
+        print("Invalid resposne of {}. Expected (y/n).".format(response))
+        exit(1)
+
+project = join(curdir, args.name)
+mkdir(project)
+
+contents = [join("common", file) for file in listdir("common")] + [join(args.type, file) for file in listdir(args.type)]
+
+for file in contents:
+    is_template = search(r"(.*)\.template", file)
+    if is_template == None:
+        copyfile(file, join(project, basename(file)))
+    else:
+        with open(file, 'r') as template, open(join(project, basename(is_template.group(1))), 'w') as asset:
+            for line in template:
+                for replacement in [(TEMPLATED_VALUES.project_name, args.name), \
+                                    (TEMPLATED_VALUES.project_desc, description)]:
+                    line = line.replace(*replacement)
+                print(line, end='', file=asset)
