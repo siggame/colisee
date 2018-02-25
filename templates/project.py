6e3e1ce67f66e07487d7c188f32c28cabdeaaff0
user404d <conduffql@gmail.com>
2018-02-25 01:13:41 -0600

    Update CHANGELOG template

--- templates/project.py
+++ templates/project.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 from collections import namedtuple
 from os import listdir, mkdir
 from os.path import basename, curdir, exists, isfile, join
-from re import search, sub
+from re import search
 from sys import exit
 from shutil import copyfile
 
@@ -12,9 +12,12 @@ Template = namedtuple("Template", ["project_name", "project_desc"])
 
 TEMPLATED_VALUES = Template(r"${PROJECT_NAME}", r"${PROJECT_DESCRIPTION}")
 
-parser = ArgumentParser(prog="colisee-project", description="Create new projects from project templates.")
+parser = ArgumentParser(
+    prog="colisee-project",
+    description="Create new projects from project templates.")
 parser.add_argument("name", help="Name for the new project")
-parser.add_argument("type", choices=["library", "service"], help="Type of the new project")
+parser.add_argument(
+    "type", choices=["library", "service"], help="Type of the new project")
 
 args = parser.parse_args()
 
@@ -41,14 +44,16 @@ while description == "":
 project = join(curdir, args.name)
 mkdir(project)
 
-contents = [join("common", file) for file in listdir("common")] + [join(args.type, file) for file in listdir(args.type)]
+contents = [join("common", file) for file in listdir("common")
+           ] + [join(args.type, file) for file in listdir(args.type)]
 
 for file in contents:
     is_template = search(r"(.*)\.template", file)
-    if is_template == None:
+    if is_template is None:
         copyfile(file, join(project, basename(file)))
     else:
-        with open(file, 'r') as template, open(join(project, basename(is_template.group(1))), 'w') as asset:
+        with open(file, 'r') as template, open(
+                join(project, basename(is_template.group(1))), 'w') as asset:
             for line in template:
                 for replacement in [(TEMPLATED_VALUES.project_name, args.name), \
                                     (TEMPLATED_VALUES.project_desc, description)]:
